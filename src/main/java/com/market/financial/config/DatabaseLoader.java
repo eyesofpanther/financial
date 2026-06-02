ef618e420b01889ef73a656f793cab5f892f6543
eyesofpanther <eyesofpanther@gmail.com>
2026-06-01 22:20:27 -0400

    Trato o bloqueo de CORS e removido findAll Asset na console

--- src/main/java/com/market/financial/config/DatabaseLoader.java
+++ src/main/java/com/market/financial/config/DatabaseLoader.java
@@ -1,33 +1,34 @@
 package com.market.financial.config;
 
-import com.market.financial.model.Asset;
+// import com.market.financial.model.Asset;
 import com.market.financial.repository.AssetRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import java.util.List;
+// import java.util.List;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-    private final AssetRepository assetRepository;
-
-    // Injeção de dependência automática pelo Spring
+    // // Injeção de dependência automática pelo Spring
     public DatabaseLoader(AssetRepository assetRepository) {
-        this.assetRepository = assetRepository;
     }
 
     @Override
+    // public void run(String... args) throws Exception {
+    //     System.out.println("\n--- INICIANDO SELECT NA TABELA ASSET ---");
+        
+    //     List<Asset> assets = assetRepository.findAll();
+        
+    //     if (assets.isEmpty()) {
+    //         System.out.println("Nenhum registro encontrado na tabela Asset.");
+    //     } else {
+    //         assets.forEach(System.out::println);
+    //     }
+        
+    //     System.out.println("----------------------------------------\n");
+    // }
+
     public void run(String... args) throws Exception {
-        System.out.println("\n--- INICIANDO SELECT NA TABELA ASSET ---");
-        
-        List<Asset> assets = assetRepository.findAll();
-        
-        if (assets.isEmpty()) {
-            System.out.println("Nenhum registro encontrado na tabela Asset.");
-        } else {
-            assets.forEach(System.out::println);
-        }
-        
-        System.out.println("----------------------------------------\n");
+        // Método limpo para evitar consultas desnecessárias no log de inicialização
     }
 }
